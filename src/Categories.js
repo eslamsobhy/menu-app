3ac9128a3d53f627825b671ed8e7e72a75e926bc
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-10-31 23:05:49 +0200

    added the filter buttons dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -1,28 +1,19 @@
 import React from "react";
 
-const Categories = ({ filterItems }) => {
+const Categories = ({ filterItems, categories }) => {
   return (
     <div className="btn-container">
-      {/*
-       there are two downsides fo adding them manually like that:
-       - first, if we add more categories, we need to add more filters manually again
-       - second, we need to update the page manually after filtering because the (state)items has been changed!
-      */}
-      <button className="filter-btn" onClick={() => filterItems("all")}>
-        all
-      </button>
-
-      <button className="filter-btn" onClick={() => filterItems("breakfast")}>
-        breakfast
-      </button>
-
-      <button className="filter-btn" onClick={() => filterItems("lunch")}>
-        lunch
-      </button>
-
-      <button className="filter-btn" onClick={() => filterItems("shakes")}>
-        shakes
-      </button>
+      {categories.map((category) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
     </div>
   );
 };
