fecc90585b459545e334b1cdede26d96f9dd32fc
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-10-31 20:41:11 +0200

    two downsides for this approach

--- src/Categories.js
+++ src/Categories.js
@@ -3,6 +3,11 @@ import React from "react";
 const Categories = ({ filterItems }) => {
   return (
     <div className="btn-container">
+      {/*
+       there are two downsides fo adding them manually like that:
+       - first, if we add more categories, we need to add more filters manually again
+       - second, we need to update the page manually after filtering because the (state)items has been changed!
+      */}
       <button className="filter-btn" onClick={() => filterItems("all")}>
         all
       </button>
